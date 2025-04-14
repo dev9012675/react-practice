77711b09e3858ac4d5af15171c632114ee2e5c22
dev9012675 <danialcs606@gmail.com>
2025-04-14 12:13:08 +0500

    Websocket Auth

--- src/providers/useWebSocket.ts
+++ src/providers/useWebSocket.ts
@@ -1,20 +1,25 @@
 import { useEffect, useRef, useState } from "react";
+import { useAuth } from "./authProvider";
 
 export const useWebSocket = (url: string) => {
   const socket = useRef<WebSocket>(null);
   const [event, setEvent] = useState<boolean>(true);
+  const { user } = useAuth();
 
   useEffect(() => {
-    socket.current = new WebSocket(url);
-    socket.current.onmessage = function () {
-      setEvent((prevEvent) => !prevEvent);
-      console.log("Event received");
-    };
+    if (typeof user !== `undefined`) {
+      socket.current = new WebSocket(url);
+      socket.current.onopen = () => socket.current?.send(user.access_token);
+      socket.current.onmessage = function () {
+        setEvent((prevEvent) => !prevEvent);
+        console.log("Event received");
+      };
 
-    return () => {
-      console.log("Connection closed");
-      socket.current?.close();
-    };
-  }, []);
+      return () => {
+        console.log("Connection closed");
+        socket.current?.close();
+      };
+    }
+  }, [url, user]);
   return event;
 };
